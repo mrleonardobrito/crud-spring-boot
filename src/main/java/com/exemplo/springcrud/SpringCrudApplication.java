20c53cf6feae18be8d755b66acfb1eee12a8e566
mrleonardobrito <lbritogit@gmail.com>
2023-12-30 22:04:46 +0000

    initial commit

---
+++ src/main/java/com/exemplo/springcrud/SpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.springcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApplication.class, args);
+	}
+
+}
